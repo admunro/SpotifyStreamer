c7407dc8265626473a916d0d73025e22be6c7410
Andy Munro <admunro@gmx.de>
2015-07-05 21:13:46 +0200

    Initial commit. Here we go...

---
+++ app/src/main/java/com/example/android/spotifystreaner/ArtistSearchFragment.java
@@ -0,0 +1,26 @@
+package com.example.android.spotifystreaner;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class ArtistSearchFragment extends Fragment
+{
+
+    public ArtistSearchFragment()
+    {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState)
+    {
+        return inflater.inflate(R.layout.fragment_artist_search, container, false);
+    }
+}
