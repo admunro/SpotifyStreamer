05aeaa5d75070be5977782f57503bdf93d8c01a8
Andy Munro <admunro@gmx.de>
2015-07-05 21:46:06 +0200

    Populate Artist Search fragment with a text input field and a scrollable list of dummy data.

--- app/src/main/java/com/example/android/spotifystreaner/ArtistSearchFragment.java
+++ app/src/main/java/com/example/android/spotifystreaner/ArtistSearchFragment.java
@@ -1,11 +1,12 @@
 package com.example.android.spotifystreaner;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 
@@ -27,11 +28,23 @@ public class ArtistSearchFragment extends Fragment
     {
         View rootView = inflater.inflate(R.layout.fragment_artist_search, container, false);
 
+        ArrayList<String> dummyData = new ArrayList<>();
+
+        // A nice old-fashioned loop to ensure that this Fragment automatically scrolls.
+        for (int i = 1; i < 20; i++)
+        {
+            dummyData.add("Artist " + Integer.toString(i));
+        }
+
         artistSearchResultsAdapter = new ArrayAdapter<> (getActivity(),
                                                          R.layout.fragment_artist_result,
                                                          R.id.fragment_artist_result_name,
-                                                         new ArrayList<String>());
+                                                         dummyData);
 
-        return inflater.inflate(R.layout.fragment_artist_search, container, false);
+        ListView listView = (ListView) rootView.findViewById(R.id.artist_search_results);
+
+        listView.setAdapter(artistSearchResultsAdapter);
+
+        return rootView;
     }
 }
