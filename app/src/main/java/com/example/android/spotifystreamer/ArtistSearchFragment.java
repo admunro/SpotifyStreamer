96bb79af1231d08eea63080712c720d21e0fb0b5
Andy Munro <admunro@gmx.de>
2015-07-11 16:26:53 +0200

    Query Spotify service for artist search

--- app/src/main/java/com/example/android/spotifystreamer/ArtistSearchFragment.java
+++ app/src/main/java/com/example/android/spotifystreamer/ArtistSearchFragment.java
@@ -1,16 +1,27 @@
 package com.example.android.spotifystreamer;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.List;
+
+import kaaes.spotify.webapi.android.SpotifyApi;
+import kaaes.spotify.webapi.android.SpotifyService;
+import kaaes.spotify.webapi.android.models.Artist;
+import kaaes.spotify.webapi.android.models.ArtistsPager;
 
 
 /**
@@ -18,6 +29,10 @@ import java.util.ArrayList;
  */
 public class ArtistSearchFragment extends Fragment
 {
+    final static String LOG_TAG = ArtistSearch.class.getSimpleName();
+
+    static SpotifyService spotifyService;
+
     private ArrayAdapter artistSearchResultsAdapter;
 
     public ArtistSearchFragment()
@@ -32,11 +47,11 @@ public class ArtistSearchFragment extends Fragment
 
         ArrayList<String> dummyData = new ArrayList<>();
 
-        // A nice old-fashioned loop to ensure that this Fragment automatically scrolls.
-        for (int i = 1; i <= 20; i++)
-        {
-            dummyData.add("Artist " + Integer.toString(i));
-        }
+//        // A nice old-fashioned loop to ensure that this Fragment automatically scrolls.
+//        for (int i = 1; i <= 20; i++)
+//        {
+//            dummyData.add("Artist " + Integer.toString(i));
+//        }
 
         artistSearchResultsAdapter = new ArrayAdapter<> (getActivity(),
                                                          R.layout.fragment_artist_result,
@@ -60,10 +75,66 @@ public class ArtistSearchFragment extends Fragment
             }
         });
 
+        final EditText textView = (EditText) rootView.findViewById(R.id.artist_search_text_input);
 
+        textView.setText("Type an artist to search for...");
 
+        // This listener will only be triggered when the Enter key is pressed
+        textView.setOnEditorActionListener(new TextView.OnEditorActionListener()
+        {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event)
+            {
+                String input = textView.getText().toString();
 
+                new SearchSpotifyTask().execute(input);
+
+                return false;
+            }
+        });
 
         return rootView;
+    }
+
+    public class SearchSpotifyTask extends AsyncTask<String, Void, ArtistsPager>
+    {
+        protected ArtistsPager doInBackground(String... strings)
+        {
+            spotifyService = new SpotifyApi().getService();
+
+            ArtistsPager results = spotifyService.searchArtists(strings[0]);
+
+            List<Artist> artists = results.artists.items;
+
+            // Check for a zero-length list - this means no artists were found
+            if (artists.size() == 0)
+            {
+                return null;
+            }
+
+            for (Artist artist : artists)
+            {
+                Log.i(LOG_TAG, artist.name);
+            }
+
+            return results;
+
+        }
+
+        protected void onPostExecute(ArtistsPager artists)
+        {
+            if (artists == null)
+            {
+                return;
+            }
+
+            artistSearchResultsAdapter.clear();
+
+            for (Artist artist : artists.artists.items)
+            {
+                artistSearchResultsAdapter.add(artist.name);
+                Log.i(LOG_TAG, "Log image: " + artist.images.get(0).url.toString());
+            }
+        }
     }
 }
